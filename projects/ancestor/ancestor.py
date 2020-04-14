2c83db7c2783aeb9bd949bd3c92f5f0169d00859
Jean Fraga <fragajean7@gmail.com>
2020-04-14 17:23:28 -0500

    finished graph II homework. Ancestor folder.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,21 @@
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+    for i in ancestors:
+        graph.add_vertex(i[0])
+        graph.add_vertex(i[1])
+    # this order lets me see the parents of every number
+    # {1: {3}, 3: {6}, 2: {3}, 6: set(), 5: {6, 7}, 7: set(), 4: {8, 5}, 8: {9}, 9: set(), 11: {8}, 10: {1}}
+    for i in ancestors:
+        graph.add_edge(i[1], i[0])
+    # print(graph.vertices)
+    answer = graph.bft(starting_node)
+    if answer == starting_node:
+        return -1
+    else:
+        return answer
+
+
+
+# print(earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)], 6))
